2f7e8a10d1b6a9903f8aaa6162c142d16f89313c
“JennyOh1004” <ohseulki1004@gmail.com>
2020-01-08 02:01:04 +0000

    Add user authentication

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,22 @@
 class ChatsController < ApplicationController
+	before_action :authenticate_user!, only: [:new, :create]
+
 	def index
+		@chats = Chat.all
+	end
+
+	def new
+		@chat = Chat.new
+	end
+
+	def create
+		current_user.chats.create(chat_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def chat_params
+		params.require(:chat).permit(:name, :description)
 	end
 end
